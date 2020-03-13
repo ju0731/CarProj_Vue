27cfc4072f0ed18b45ad3be12139e708d91b80b7
ju0731 <60119657+ju0731@users.noreply.github.com>
2020-03-13 14:55:07 +0900

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,8 +9,6 @@ module.exports = {
     },
     rules: {
         "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-        "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-        "semi": ["error", "always"],
-        "quotes": ["error", "double"]
+        "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
     }
 };
