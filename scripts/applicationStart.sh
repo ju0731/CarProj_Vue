f0ba25099ad645dcfa245a70f045029952ab0de1
woodchuckchoi <choiggoggi@gmail.com>
2020-03-15 21:38:58 +0900

    router service added

--- scripts/applicationStart.sh
+++ scripts/applicationStart.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo mv /home/ec2-user/front/scripts/devops_front.service /etc/systemd/system/
+sudo mv /home/ec2-user/front/scripts/devops_router.service /etc/systemd/system/
 sudo systemctl daemon-reload
-sudo systemctl start devops_front.service
-sudo systemctl enable devops_front.service
+sudo systemctl start devops_front.service; sudo systemctl start devops_router.service
+sudo systemctl enable devops_front.service; sudo systemctl enable devops_router.service
