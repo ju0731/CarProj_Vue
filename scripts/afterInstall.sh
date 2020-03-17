6f903c204712fdabc0a786490b50da43f0357b8b
woodchuckchoi <choiggoggi@gmail.com>
2020-03-17 18:11:35 +0900

    hook sequence modified

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 cd /home/ec2-user/front
 npm install webpack --save-dev
-cp url.json ./src/asset/url.json
--- scripts/applicationStart.sh
+++ scripts/applicationStart.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo mv /home/ec2-user/front/url.json /home/ec2-user/front/src/asset/url.json
 sudo mv /home/ec2-user/front/scripts/devops_front.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl start devops_front.service
