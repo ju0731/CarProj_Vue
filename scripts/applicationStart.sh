d88532448e07545529db71ca26ce2a599fee8f8d
woodchuckchoi <choiggoggi@gmail.com>
2020-03-17 18:23:19 +0900

    hook sequence modified

--- scripts/applicationStart.sh
+++ scripts/applicationStart.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sudo mv /home/ec2-user/front/url.json /home/ec2-user/front/src/asset/url.json
 sudo mv /home/ec2-user/front/scripts/devops_front.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl start devops_front.service
