b461024b5c8b461b39ef47674ffbddfaa7b44bc8
woodchuckchoi <choiggoggi@gmail.com>
2020-03-17 14:54:04 +0900

    checking public ip method modified

--- scripts/generate_config.sh
+++ scripts/generate_config.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 URL=$(jq '.url' /home/ec2-user/front/src/assets/url.json -r)
-MYIP=$(dig +short myip.opendns.com @resolver1.opendns.com)
-jq -n --arg URL $URL --arg MYIP $MYIP '{url: $URL, myip: $MYIP}' > /home/ec2-user/front/src/assets/url.json
+MYIP=$(wget -qO- icanhazip.com)
+jq -n --arg URL "$URL" --arg MYIP "$MYIP" '{url: $URL, myip: $MYIP}' > /home/ec2-user/front/src/assets/url.json
