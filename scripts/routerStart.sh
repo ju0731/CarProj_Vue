14f23206a5e48ea536ff96e584ddf02225dd7b1c
woodchuckchoi <choiggoggi@gmail.com>
2020-03-25 15:12:08 +0900

    systemd service restart at every hook

--- scripts/routerStart.sh
+++ scripts/routerStart.sh
@@ -3,3 +3,4 @@ sudo mv /home/ec2-user/front/scripts/devops_router.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl start devops_router.service
 sudo systemctl enable devops_router.service
+sudo systemctl restart devops_router.service
